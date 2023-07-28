db1a92ce31b490ee290b47c531884f6ed58efc10
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-07-29 05:01:24 +1000

    showing active user detail to redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,27 @@
 import { auth, provider } from "../firebase";
+import { SET_USER } from "./actionType";
+
+export const setUser = (payload) => ({
+  type: SET_USER,
+  user: payload,
+});
 
 export function signInAPI() {
   return (dispatch) => {
     auth
       .signInWithPopup(provider)
       .then((payload) => {
-        console.log(payload);
+        dispatch(setUser(payload.user));
       })
       .catch((error) => alert(error.message));
+  };
+}
+export function getUserAuth() {
+  return (dispatch) => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        dispatch(setUser(user));
+      }
+    });
   };
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,12 @@ import styled from "styled-components";
 import React from "react";
 import { connect } from "react-redux";
 import { signInAPI } from "../actions/index";
+import { Navigate } from "react-router-dom";
 
 const Login = (props) => {
   return (
     <Container>
+      {props.user && <Navigate to="/home" />}
       <Nav>
         <a href="/">
           <img src="/images/login-logo.svg" alt="" />
@@ -165,7 +167,9 @@ const Google = styled.button`
   }
 `;
 const mapStateToProps = (state) => {
-  return {};
+  return {
+    user: state.userState.user,
+  };
 };
 const mapDispatchToProps = (dispatch) => ({
   signIn: () => {
